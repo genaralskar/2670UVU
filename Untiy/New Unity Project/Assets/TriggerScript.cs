df76902154dc8d28b495cbc21b110c3680eb8a6d
Unknown <genaralskar1@gmail.com>
2017-08-23 14:03:16 -0600

    add unity trigger stuff

---
+++ Untiy/New Unity Project/Assets/TriggerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+		print("Enter");
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit () {
+		print("Exit");
+	}
+}
