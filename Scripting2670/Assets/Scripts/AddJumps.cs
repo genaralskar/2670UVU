cc08594e2c2ceb0bfa86ee10b2fe02bdd1988746
Unknown <genaralskar1@gmail.com>
2017-09-04 17:13:36 -0600

    stuff

---
+++ Scripting2670/Assets/Scripts/AddJumps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddJumps : MonoBehaviour {
+
+	public int jumpsToAdd;
+	MoveCharacter chara;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+		chara = other.GetComponent<MoveCharacter>();
+		chara.jumpAmount += jumpsToAdd;
+		print("curJumps = " + chara.jumpAmount);
+		}
+	}
+}
