403aed1331a98afd847fae04d49c67cc796b5dc0
Unknown <genaralskar1@gmail.com>
2017-09-05 12:46:33 -0600

    character can push objects

---
+++ Scripting2670/Assets/Scripts/BandFPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandFPlatform : MonoBehaviour {
+
+	public float moveSpeed = 2;
+	public float maxDistance = 5;
+	public float curDistance = 0;
+	public Vector3 vel;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		MoveInput.ConstantAction += MovePlatform;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void MovePlatform()
+	{
+		vel = rb.velocity;
+		transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+		curDistance += Time.deltaTime;
+		if(curDistance >= maxDistance)
+		{
+			moveSpeed *= -1;
+			curDistance = 0;
+		}
+	}
+}
---
+++ Scripting2670/Assets/Scripts/PushRigd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushRigd : MonoBehaviour {
+
+	public float pushPower = 2;
+
+	void OnControllerColliderHit (ControllerColliderHit hit)
+	{
+		Rigidbody rb = hit.collider.attachedRigidbody;
+		if(rb == null || rb.isKinematic)
+			return;
+
+		if(hit.moveDirection.y < -.3f)
+			return;
+
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, rb.velocity.y, hit.moveDirection.z);
+		rb.velocity += pushDir * pushPower;
+	}
+
+}
