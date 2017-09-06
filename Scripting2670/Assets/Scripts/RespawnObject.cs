4d7438c7b574f37c2b38823f3a2a82b609ff8dc1
Unknown <genaralskar1@gmail.com>
2017-09-06 11:53:52 -0600

    updates

---
+++ Scripting2670/Assets/Scripts/RespawnObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObject : MonoBehaviour {
+
+	public Vector3 respawnLoc;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Trigger")
+		{
+			other.transform.position = respawnLoc;
+		}
+	}
+}
