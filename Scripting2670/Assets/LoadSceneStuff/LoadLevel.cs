69ae8b1c904139301a81d16584740a1e48870d63
Unknown <genaralskar1@gmail.com>
2017-09-08 15:01:53 -0600

    load level stuff

---
+++ Scripting2670/Assets/LoadSceneStuff/LoadLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour {
+
+	public string sceneName;
+	public static Action EndButton;
+
+	void Start ()
+	{
+		LoadLevel.EndButton += DisableButton;
+	}
+
+	public void Load()
+	{
+		EndButton();
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+	}
+
+	public void DisableButton()
+	{
+		GetComponent<Button>().interactable = false;
+	}
+
+
+}
