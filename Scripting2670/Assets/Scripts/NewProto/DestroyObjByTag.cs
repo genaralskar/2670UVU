ddf61227812fdc225aeb60085595984ec8d00a26
Unknown <genaralskar1@gmail.com>
2017-09-16 20:26:48 -0600

    Update level prototype
    logs now spawn instead of teleport, added more move scripts, tweaked level layout

---
+++ Scripting2670/Assets/Scripts/NewProto/DestroyObjByTag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjByTag : MonoBehaviour {
+
+	public string tagg;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == tagg)
+		{
+			for(int i = 0; i < other.transform.childCount; i++)
+			{
+				if(other.transform.GetChild(i).tag == "Player")
+				{
+					other.transform.GetChild(i).parent = null;
+				}
+			}
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Scripting2670/Assets/Scripts/NewProto/SpawnObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+	public GameObject spawnObj;
+	public float spawnTime;
+	public bool runOnStart;
+
+	// Use this for initialization
+	void Start () {
+		if(runOnStart)
+		{
+			StartCoroutine(Spawn());
+		}
+	}
+	
+	IEnumerator Spawn()
+	{
+		while(true)
+		{
+			Instantiate(spawnObj, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(spawnTime);
+		}
+	}
+}
