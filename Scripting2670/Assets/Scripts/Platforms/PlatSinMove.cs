638d9f17c2e24099fda5fc15deb7a5809975ac20
Unknown <genaralskar1@gmail.com>
2017-10-01 22:31:03 -0600

    new opening animation, add moving ladder to game

---
+++ Scripting2670/Assets/Scripts/Platforms/PlatSinMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatSinMove : MonoBehaviour {
+
+	public float horizSinVar = 1;
+	public float horizSpeed;
+	public float vertSinVar = 0;
+	public float vertSpeed = 0;
+	public float offset;
+	public bool runAtStart = true;
+	Vector3 startPos;
+
+	void Start()
+	{
+		startPos = transform.position;
+		if(runAtStart)
+		{
+			StartCoroutine(Move());
+		}
+	}
+
+	void StartMove()
+	{
+
+	}
+	
+	void StopMove()
+	{
+
+	}
+
+	IEnumerator Move()
+	{
+		while(true)
+		{
+			yield return null;
+			transform.position = new Vector3(startPos.x + Mathf.Sin(Time.time + (offset * vertSpeed)) * vertSinVar, startPos.y + Mathf.Sin(Time.time + (offset * horizSpeed)) * horizSinVar, transform.position.z);
+		}
+	}
+}
