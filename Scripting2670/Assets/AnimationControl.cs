2a96a34f5ae0b9c53a4550ab693c9fdcdde9a28a
Unknown <genaralskar1@gmail.com>
2017-11-06 15:19:47 -0700

    update game animations

---
+++ Scripting2670/Assets/AnimationControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour {
+
+	public Animator anim;
+
+	void Update()
+	{
+		anim.SetFloat("Walk", Mathf.Abs(Input.GetAxis("Horizontal")));
+	}
+}
